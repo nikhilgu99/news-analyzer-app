4f7cc9c505c0cbcacb86ac7b83b63fcbdb125d07
Nikhil Gupta <99nikhilgupta@gmail.com>
2021-04-24 00:40:19 -0400

    Extract/Display descriptions from articles

--- news.py
+++ news.py
@@ -4,9 +4,18 @@ from urllib.request import urlopen
 
 def getNews(publisher, keyword):
     keyword = keyword.replace(" ", "%20")
-    url = "https://newsapi.org/v2/everything?q=" + keyword + "&apiKey=8916674ee011411aae3f5d83992abd18"
+    url = "https://newsapi.org/v2/everything?sources=" + publisher + "&q=" + keyword + "&apiKey=8916674ee011411aae3f5d83992abd18"
 
-    jsonurl = urlopen(url)
+    # Get the returned json and pull the info we need
+    jsonurl = urlopen(url) 
     fulltext = json.loads(jsonurl.read())
 
-    return fulltext
+    if fulltext["totalResults"] == 0:
+        return "NO ARTICLES FOUND!"
+
+    retVal = ""
+    for article in fulltext["articles"]:
+        description = article["description"]
+        retVal += (description + "<br>")
+
+    return retVal
